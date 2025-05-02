e6ef9cafded09ef6f176dcf4d97da2965d74014f
Benjamin DalFavero <bdalfavero@gmail.com>
2025-05-02 09:35:23 -0400

    Added bound to calculation.

--- hubbard_test_run.sh
+++ hubbard_test_run.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-tau=1e-1
-steps=100
-d=16
+tau=1e-2
+steps=1000
+d=25
 eps=1e-12
 
+python hubbard_exact.py
 python hubbard_subspace_matrices.py $tau $steps $d hubbard_subspace.hdf5
 python hubbard_eigenvalues.py hubbard_subspace.hdf5 hubbard_eigenvalues.hdf5 $eps
+python hubbard_bound.py hubbard_exact.h5 hubbard_subspace.hdf5 hubbard_eigenvalues.hdf5 hubbard_bound.hdf5 
