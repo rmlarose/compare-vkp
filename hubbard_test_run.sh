d69ec3fd9154e305f9e7827de3a8e7d174e424b5
Benjamin DalFavero <bdalfavero@gmail.com>
2025-05-27 09:21:55 -0400

    Changed parameters to run.

--- hubbard_test_run.sh
+++ hubbard_test_run.sh
@@ -7,7 +7,7 @@ tau=1e-1
 steps=300
 d=16
 eps=1e-9
-ratio=0.1
+ratio=0.3
 
 python hubbard_exact.py $n hubbard_exact.hdf5
 python hubbard_subspace_matrices.py $n $tau $steps $d $ratio hubbard_exact.hdf5 hubbard_subspace.hdf5
