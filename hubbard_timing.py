69388fb2e83a65f6d29dee172786983e8e5642ec
Benjamin DalFavero <bdalfavero@gmail.com>
2025-09-19 09:21:59 -0400

    Script to time contraction for the Hubbard model.

---
+++ hubbard_timing.py
@@ -0,0 +1,77 @@
+from time import process_time_ns
+import numpy as np
+import scipy.linalg as la
+import torch
+import cirq
+import qiskit
+import openfermion as of
+import qiskit
+from qiskit.qasm2 import dumps
+import quimb
+from quimb.tensor.circuit import CircuitMPS
+import krylov_common as kc
+from convert import cirq_pauli_sum_to_qiskit_pauli_op
+
+def to_torch(x):
+    # We use 'mps' for the Macbook. Use 'cuda' for HPCC.
+    return torch.tensor(x, dtype=torch.complex64, device="cuda")
+
+def main():
+    l = 2
+    t = 1.0
+    u = 6.0
+    ham_fermi = of.hamiltonians.fermi_hubbard(l, l, t, u, spinless=True)
+    hamiltonian = of.transforms.jordan_wigner(ham_fermi)
+    ham_cirq = of.transforms.qubit_operator_to_pauli_sum(hamiltonian)
+    nq = of.count_qubits(hamiltonian)
+    qs = cirq.LineQubit.range(nq)
+
+    print("Compiling circuit.")
+    steps = 1
+    tau = 0.1
+    compile_start_time = process_time_ns()
+    ham_paulisum = of.transforms.qubit_operator_to_pauli_sum(hamiltonian)
+    ham_qiskit = cirq_pauli_sum_to_qiskit_pauli_op(ham_paulisum)
+    dt = tau / float(steps)
+    ev_gate = qiskit.circuit.library.PauliEvolutionGate(ham_qiskit, time=dt)
+    ev_ckt_qiskit = qiskit.QuantumCircuit(nq)
+    for _ in range(steps):
+        ev_ckt_qiskit.append(ev_gate, range(nq))
+    compile_end_time = process_time_ns()
+    compile_elapsed_time = compile_end_time - compile_start_time
+    transpiled_circuit = qiskit.transpile(ev_ckt_qiskit, basis_gates=["u3", "cx"])
+    print(f"Time to compile {compile_elapsed_time:1.6e}")
+    print(f"Circuit depth {transpiled_circuit.depth()}")
+
+    reference_circuit = qiskit.QuantumCircuit(nq)
+    n_occ = 2
+    for i in range(nq):
+        if i < n_occ:
+            reference_circuit.x(i)
+    ref_circuit_qasm = dumps(reference_circuit)
+    quimb_circuit = CircuitMPS.from_openqasm2_str(ref_circuit_qasm)
+    reference_mps = quimb_circuit.psi
+    for tensor in reference_mps.tensors:
+        tensor.modify(apply=lambda x: to_torch(x))
+    assert len(reference_mps.tensor_map) == nq
+
+    max_circuit_bond = 64
+    max_mpo_bond = 80
+    ham_mpo = quimb.load_from_disk("phosphate_mpo_chi597.data")
+    for tensor in ham_mpo.tensors:
+        tensor.modify(apply=lambda x: to_torch(x))
+    d = 1
+    print("Computing overlap and matrix element.")
+    print(f"d = {d} Max circuit bond dim = {max_circuit_bond} Max MPO bond = {max_mpo_bond}")
+    contract_start_time = process_time_ns()
+    kc.tebd_matrix_element_and_overlap(
+        ham_mpo, transpiled_circuit, reference_mps,
+        d, max_circuit_bond, to_torch
+    )
+    contract_end_time = process_time_ns()
+    contract_elapsed_time = contract_end_time - contract_start_time
+    print(f"Time to contract {contract_elapsed_time:1.6e}")
+
+
+if __name__ == "__main__":
+    main()
