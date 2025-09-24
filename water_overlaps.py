ee8eb61eee1efe96ed71a2a194b7ee8eb545f94d
Benjamin DalFavero <bdalfavero@gmail.com>
2025-09-24 09:40:11 -0400

    Added a script that computes single overlaps for the water Hamiltonian.

---
+++ water_overlaps.py
@@ -0,0 +1,126 @@
+from typing import List
+import argparse
+import pickle
+import json
+from mpi4py import MPI
+import h5py
+import numpy as np
+import scipy.linalg as la
+import torch
+import quimb
+import cirq
+from cirq.contrib.qasm_import import circuit_from_qasm
+import qiskit
+import openfermion as of
+import qiskit
+from qiskit import qpy
+from qiskit.qasm2 import dumps
+from quimb.tensor.tensor_1d import MatrixProductState
+from quimb.tensor.circuit import CircuitMPS
+from kcommute import get_si_sets
+from trotter_circuit import trotter_multistep_from_groups
+import krylov_common as kc
+from convert import cirq_pauli_sum_to_qiskit_pauli_op
+from tensor_network_common import pauli_sum_to_mpo
+
+def to_torch(x, device_name: str="cuda"):
+    return torch.tensor(x, dtype=torch.complex64, device=device_name)
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input_file", type=str, help="JSON input file with parameters.")
+    parser.add_argument("output_file", type=str, help="Output filename.")
+    args = parser.parse_args()
+
+    print("Reading input.")
+    with open(args.input_file) as f:
+        input_dict = json.load(f)
+    n_occ = input_dict["n_occ"] # Number of occupied orbitals.
+    steps = input_dict["steps"]
+    d = input_dict["d"]
+    tau = input_dict["tau"]
+    max_circuit_bond = input_dict["max_circuit_bond"]
+    max_mpo_bond = input_dict["max_mpo_bond"]
+    hamiltonian_mpo_filename = input_dict["mpo_filename"] # This is for reading an MPO.
+    hamiltonian_file = input_dict["hamiltonian_file"]
+
+    comm = MPI.COMM_WORLD
+    mpi_comm_rank = comm.Get_rank()
+    mpi_comm_size = comm.Get_size()
+
+    # threshold_tolerance: float = 1e-2
+    # ham_fermi = of.utils.load_operator(file_name=hamiltonian_file, data_directory=hamiltonian_directory)
+    # hamiltonian = of.transforms.jordan_wigner(ham_fermi)
+    hamiltonian = of.jordan_wigner(
+            of.get_fermion_operator(
+        of.chem.MolecularData(filename=hamiltonian_file).get_molecular_hamiltonian()
+        )
+    )
+    # hamiltonian.compress(abs_tol=threshold_tolerance)
+    ham_cirq = of.transforms.qubit_operator_to_pauli_sum(hamiltonian)
+    nq = of.utils.count_qubits(hamiltonian)
+    qs = cirq.LineQubit.range(nq)
+    ham_mpo = quimb.load_from_disk(hamiltonian_mpo_filename)
+    nq = len(ham_mpo.tensors)
+    qs = cirq.LineQubit.range(nq)
+
+    print("Compiling circuits.")
+    # Get the first-order Trotter circuit.
+    use_paulihedral = True
+    ham_paulisum = of.transforms.qubit_operator_to_pauli_sum(hamiltonian)
+    if use_paulihedral:
+        ham_qiskit = cirq_pauli_sum_to_qiskit_pauli_op(ham_paulisum)
+        dt = tau / float(steps)
+        ev_gate = qiskit.circuit.library.PauliEvolutionGate(ham_qiskit, time=dt)
+        ev_ckt_qiskit = qiskit.QuantumCircuit(nq)
+        for _ in range(steps):
+            ev_ckt_qiskit.append(ev_gate, range(nq))
+    else:
+        assert len(ham_paulisum.qubits) == nq
+        assert set(ham_paulisum.qubits).issubset(set(qs))
+        tau = tau
+        steps = steps
+        dt = tau / float(steps)
+        groups = get_si_sets(ham_paulisum, k=1)
+        with open("hubbard_groups.pkl", "wb") as f:
+            pickle.dump(groups, f)
+        evolution_ckt = trotter_multistep_from_groups(
+            groups, qs, tau, steps
+        )
+        ev_ckt_qasm = evolution_ckt.to_qasm()
+        ev_ckt_qiskit = qiskit.QuantumCircuit.from_qasm_str(ev_ckt_qasm)
+    # with open("hubbard_trotter_ckt.qpy", "wb") as f:
+    #     qpy.dump(ev_ckt_qiskit, f)
+
+    # We will use a Hartree-Fock state as our reference.
+    reference_circuit = qiskit.QuantumCircuit(nq)
+    for i in range(nq):
+        if i < n_occ:
+            reference_circuit.x(i)
+        # else:
+        #     reference_circuit.id(i)
+    # ref_state = qiskit.quantum_info.Statevector(reference_circuit).data
+    # ref_state_energy = hamiltonian.expectation_from_state_vector(ref_state)
+    ref_circuit_qasm = dumps(reference_circuit)
+    quimb_circuit = CircuitMPS.from_openqasm2_str(ref_circuit_qasm)
+    reference_mps = quimb_circuit.psi
+    assert len(reference_mps.tensor_map) == nq
+
+    print(f"Untranspiled circuit has depth {ev_ckt_qiskit.depth()}")
+    ev_ckt_transpiled = qiskit.transpile(ev_ckt_qiskit, basis_gates=["u3", "cx"])
+    print(f"Transpiled circuit has depth {ev_ckt_transpiled.depth()}")
+
+    # Compute the subspace matrices.
+    mat_elem, overlap = kc.tebd_matrix_element_and_overlap(
+        ham_mpo, ev_ckt_transpiled, reference_mps,
+        d, max_circuit_bond, to_torch
+    )
+
+    output_dict = {
+        "input": input_dict,
+        "overlap": overlap,
+        "mat_elem": mat_elem,
+    }
+    
+    with open(args.output_file, "w") as f:
+        json.dump(output_dict, f)
